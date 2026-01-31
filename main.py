38bec389fa2385ceedd111beacc57c2c6d1c9850
Baannaannaa <Matthieu.espra@gmail.com>
2026-02-01 00:37:19 +0100

    codage image operationel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PIL import Image
 
+
 def recupererpixel(a,img):
     largeur_image=1920
     hauteur_image=1080
@@ -10,20 +11,86 @@ def recupererpixel(a,img):
     print("fin")
     return a
 
-def cree_image(a):
+
+def cree_image(a,nom):
     nouvelle_image = Image.new('RGB', (1920, 1080))
     largeur_image=1920
     hauteur_image=1080
     for y in range(hauteur_image):
         for x in range(largeur_image):
             nouvelle_image.putpixel((x,y),(a[x][y][0],a[x][y][1],a[x][y][2]))
-    nouvelle_image.save("test.jpg")
-    
+    nouvelle_image.save(nom)
+
+
+def tab_int_to_bin(a):
+    largeur_image=1920
+    hauteur_image=1080
+    for y in range(hauteur_image):
+        for x in range(largeur_image):
+            for j in range(3):
+                a[x][y][j]= format(a[x][y][j], '08b')
+    return a
+
+
+def tab_bin_to_int(a):
+    largeur_image=1920
+    hauteur_image=1080
+    for y in range(hauteur_image):
+        for x in range(largeur_image):
+            for j in range(3):
+                a[x][y][j]= int(a[x][y][j], 2)
+    return a
+
+
+def merge_tab_bin(tabv,tabc):
+    largeur_image=1920
+    hauteur_image=1080
+    for y in range(hauteur_image):
+        for x in range(largeur_image):
+            for j in range(3):
+                tabv[x][y][j]=tabv[x][y][j][:4] + tabc[x][y][j][:4]
+    return tabv
+
+
+def decodebin(tab):
+    largeur_image=1920
+    hauteur_image=1080
+    for y in range(hauteur_image):
+        for x in range(largeur_image):
+            for j in range(3):
+                tab[x][y][j]=tab[x][y][j][4:] + "0000"
+    return tab
+
+
 def main():
-    a = [[[0,0,0] for j in range(1080)] for i in range(1920)]
-    imgv = Image.open("/home/matt/Code/stegano/stegano/image_de_base/image_visible/cars.jpg")
-    a = recupererpixel(a,imgv)
-    cree_image(a)
+    tabimgv = [[[0,0,0] for j in range(1080)] for i in range(1920)]
+    tabimgc = [[[0,0,0] for j in range(1080)] for i in range(1920)]
+
+    imgv = Image.open("image_de_base/image_visible/cars.jpg")
+    imgc = Image.open("image_de_base/image_cache/madagascar.jpg")
+
+    tabimgv = recupererpixel(tabimgv,imgv)
+    tabimgc = recupererpixel(tabimgc,imgc)
+
+    tabimgv = tab_int_to_bin(tabimgv)
+    tabimgc = tab_int_to_bin(tabimgc)
+
+    tabimgv = merge_tab_bin(tabimgv,tabimgc)
+
+    tabimgv = tab_bin_to_int(tabimgv)
+
+    cree_image(tabimgv,"code.jpg")
+
+    #decoder l'image
+    tabimgcode = [[[0,0,0] for j in range(1080)] for i in range(1920)]
+    imgcode = Image.open("/home/matt/Code/stegano/stegano/code.jpg")
+    tabimgcode = recupererpixel(tabimgcode, imgcode)
+    tabimgcode = tab_int_to_bin(tabimgv)
+    tabimgcode = decodebin(tabimgcode)
+    tabimgcode = tab_bin_to_int(tabimgcode)
+
+    cree_image(tabimgcode,"decode.jpg")
+    
     return 0
 
 
